3dbd4276fd3188ab65ed73e5e5342b93bb909d7d
navnit0707 <knavnit81@gmail.com>
2022-09-17 19:55:39 +0530

    added event listner to show the drag start end

--- dragDropProject/main.js
+++ dragDropProject/main.js
@@ -1,1 +1,10 @@
+const imgBox = document.querySelector('.imgBox');
+const whiteBoxes = document.getElementsByClassName('whiteBoxes');
 
+imgBox.addEventListener('dragstart', ()=>{
+    console.log('DragStart has been triggered');
+})
+
+imgBox.addEventListener('dragend', ()=>{
+    console.log('DragEnd triggered');
+})
