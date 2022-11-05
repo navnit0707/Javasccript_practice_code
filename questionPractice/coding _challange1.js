e82a257503fb911cf0c710816f6a3ef697910b68
navnit0707 <knavnit81@gmail.com>
2022-11-05 23:26:31 +0530

    javascript challange

--- questionPractice/coding _challange1.js
+++ questionPractice/coding _challange1.js
@@ -85,3 +85,12 @@ printGoals(...game.scored);
 team1 < team2 && console.log('Team 1 is more likely to win');
 team1 > team2 && console.log('Team 2 is more likely to win');
   
+
+
+const details ={
+  name: "navnit",
+  online:{
+    mon : true
+  }
+};
+Object.keys(details);
