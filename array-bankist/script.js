270461d1be84fa58eccd447178bcae0d5333081f
navnit0707 <knavnit81@gmail.com>
2022-12-29 12:28:35 +0530

    created a username and added it to the account so that i dont have to keep it in a seperate variable

--- array-bankist/script.js
+++ array-bankist/script.js
@@ -65,14 +65,13 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // exercises
 
-
-const displayMovements = function(movements){
-containerMovements.innerHTML= '';
-  movements.forEach(function(mov, i ){
-    const type = mov> 0 ? 'deposit' : 'withdrawal';
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class="movements__row">
-    <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
     
     <div class="movements__value">${mov}</div>
     </div>
@@ -80,7 +79,19 @@ containerMovements.innerHTML= '';
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
-
 };
 
 displayMovements(account1.movements);
+
+const createUsername = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .LowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+    return username;
+  });
+};
+
+createUsername(accounts);
